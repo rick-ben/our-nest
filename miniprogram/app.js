15b2a558ca9a255a1402fb47199164acdd88c919
lingyun <1848741639@qq.com>
2022-02-14 10:26:49 +0800

    20220214102659AM

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -30,6 +30,28 @@ App({
     wx.onThemeChange((themeResult) => {
       _this.themeChanged(themeResult.theme)
     })
+    
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+    })
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已准备好，请重启应用',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    })
+
   },
   /**
    * 改变主题
