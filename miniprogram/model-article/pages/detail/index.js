6d2ae88e4d4f11b138999bf65fd69178a922cb79
lingyun <1848741639@qq.com>
2022-05-14 22:03:01 +0800

    20220514220417PM

--- miniprogram/model-article/pages/detail/index.js
+++ miniprogram/model-article/pages/detail/index.js
@@ -1,11 +1,12 @@
 // article/pages/detail/index.js
-const { modal, format, setString } = require('../../../utils/util');
+const { modal, format, setString, toast, loading, stringLength } = require('../../../utils/util');
 
 // 连接云数据库
 const db = wx.cloud.database();
 // 获取集合的引用
 const dbArticle = db.collection('article');
 const users = db.collection('users');
+const dbArticleComment = db.collection('articleComment');
 // 数据库操作符
 const _ = db.command;
 Page({
@@ -20,7 +21,13 @@ Page({
       content: ""
     },
     showImageIndex: 0,
-    showGallery: false
+    showGallery: false,
+    commentLoadMoreStatus: 'more',
+    commentFooterTip: '',
+    comment: '',
+    commentPage: 0,
+    commentList: [],
+    commentTotal: 0
   },
 
   /**
@@ -28,6 +35,15 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  /**
+   * 下拉触底
+   */
+  onReachBottom: function () {
+    if (this.data.commentLoadMoreStatus == 'more') {
+      this.loadComments();
+    }
   },
 
   /**
@@ -69,9 +85,105 @@ Page({
         console.log(err)
         modal('提示', '获取文章信息失败');
       })
+      this.loadComments();
     } else {
       modal('非法参数', '请尝试重启小程序');
     }
+  },
+
+  /**
+   * 加载评论
+   */
+  loadComments(reload = false){
+    let _this = this;
+    let page = this.data.commentPage;
+    if (reload) {
+      this.setData({
+        commentPage: 0,
+        commentList: [],
+        commentTotal: 0
+      })
+      page = 0;
+    }
+    
+    if (!this.data.userInfo.auth_view && page == 1) {
+      this.setData({
+        commentFooterTip: '游客只能查看最新20条评论',
+        commentLoadMoreStatus: 'nomore'
+      })
+      return modal('提示','游客只能查看最新20条评论');
+    }
+    this.setData({
+      commentLoadMoreStatus: 'loading'
+    })
+    let size = 20;
+    let skip = page * size;
+    dbArticleComment.where({
+      aid: _this.data.options.id,
+      is_del: false
+    }).limit(size)
+    .skip(skip)
+    .orderBy('create_time', 'desc')
+    .get().then((res) => {
+      let list = res.data;
+      if (list.length >= 1) {
+        let selectNum = 0;
+        let dataLen = list.length;
+        let setinfo = {};
+        list.forEach(element => {
+          element.time = format(element.create_time, "yyyy-MM-dd HH:mm")
+          users.where({
+            _openid: element._openid
+          })
+            .limit(1)
+            .get()
+            .then(ret => {
+              if (ret.data.length >= 1) {
+                element.user = ret.data[0];
+              }
+              selectNum += 1;
+            })
+        });
+        if (selectNum == dataLen) {
+          applyData(_this, list);
+        } else {
+          let watchSelect = setInterval(() => {
+            if (selectNum == dataLen) {
+              applyData(_this, list);
+              clearInterval(watchSelect);
+            }
+          }, 500);
+        }
+        function applyData (_this, list){
+          if (page == 0) {
+            setinfo['commentList'] = list;
+          } else {
+            setinfo['commentList'] = [..._this.data.commentList, ...list];
+          }
+          setinfo['commentPage'] = page += 1;
+          setinfo['commentLoadMoreStatus'] = 'more';
+          _this.setData(setinfo);
+        }
+      } else {
+        if (page != 0) {
+          toast("没有更多了");
+        }
+        _this.setData({
+          commentFooterTip: '没有更多了',
+          commentLoadMoreStatus: 'nomore'
+        })
+      }
+    }).catch((e) => {
+      toast("数据加载失败", e);
+    });
+    dbArticleComment.where({
+      aid: _this.data.options.id,
+      is_del: false
+    }).count().then(res=>{
+      this.setData({
+        commentTotal: res.total
+      })
+    })
   },
 
   /**
@@ -116,5 +228,45 @@ Page({
       title: this.data.shareTitle,
       imageUrl: this.data.shareImage
     }
+  },
+
+  /**
+   * 输入评论内容
+   * @param {*} event 
+   */
+  changeCommentCnt(event){
+    this.setData({
+      comment: event.detail.value
+    })
+  },
+
+  /**
+   * 提交评论
+   */
+  submitComment(){
+    let _this = this;
+    if (stringLength(this.data.comment) <= 0) {
+      modal('提示','请先输入评论内容');
+    }
+    loading('正在提交评论');
+    let commentData = {
+      aid: this.data.options.id,
+      comment: this.data.comment,
+      create_time: db.serverDate(),
+      is_del: false
+    };
+    dbArticleComment.add({
+      data: commentData
+    }).then(res=>{
+      wx.hideLoading();
+      toast('评论成功');
+      _this.setData({
+        comment: ''
+      })
+      _this.loadComments(true);
+    }).catch(err=>{
+      wx.hideLoading();
+      toast('评论失败，请重试');
+    })
   }
 })
